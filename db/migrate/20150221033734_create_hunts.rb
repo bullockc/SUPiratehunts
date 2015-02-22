4e8eee77bc8eee6cd5ec174c3798fc0c09eee222
Jake Armando Balderama <balderaj@southwestern.edu>
2015-02-22 16:56:15 -0600

    edits to the model
    new model also. i changed the first model to lower case and corrected
    one value. thought i already did this but saved the wrong file

--- db/migrate/20150221033734_create_hunts.rb
+++ db/migrate/20150221033734_create_hunts.rb
@@ -3,15 +3,15 @@ class CreateHunts < ActiveRecord::Migration
     create_table :hunts do |t|
 
 	t.string :ID
-	t.string :Hunt_Name
-	t.string :Owner
-	t.array :Tasks
-	t.integer :Status
-	t.availability :boolean
-	t.datetime :Start_Date
-	t.datetime :End_Date
-	t.string :Start_Location
-	t.text :Description
+	t.string :hunt_name
+	t.string :owner
+	t.array :tasks
+	t.integer :status
+	t.boolean :availability
+	t.datetime :start_date
+	t.datetime :end_date
+	t.string :start_location
+	t.text :description
 	t.timestamps null: false
     end
   end
