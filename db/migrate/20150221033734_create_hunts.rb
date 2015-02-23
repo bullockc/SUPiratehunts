d6193a1a5844863e96948f47fc7fc8ec1cff413d
Jake Armando Balderama <balderaj@southwestern.edu>
2015-02-22 22:21:09 -0600

    still updating
    doing this again

--- db/migrate/20150221033734_create_hunts.rb
+++ db/migrate/20150221033734_create_hunts.rb
@@ -2,16 +2,18 @@ class CreateHunts < ActiveRecord::Migration
   def change
     create_table :hunts do |t|
 
-	t.string :ID
-	t.string :hunt_name
+	t.string :huntID
+	t.string :huntname
 	t.string :owner
-	t.index :tasks
+	t.string :tasks
 	t.integer :status
 	t.boolean :availability
-	t.datetime :start_date
-	t.datetime :end_date
-	t.string :start_location
+	t.datetime :startdate
+	t.datetime :enddate
+	t.string :startlocation
 	t.text :description
+
+
 	t.timestamps null: false
     end
   end
