63165268d267c7abb7642dbafcf610ee9d156ab6
yesnatalia <nataliaa.r@hotmail.com>
2015-03-02 20:50:38 -0600

    Update 20150227054322_create_questions.rb

--- db/migrate/20150227054322_create_questions.rb
+++ db/migrate/20150227054322_create_questions.rb
@@ -2,8 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
     	t.string :question
-		t.string :right_answer
-		t.string :submitted_answer
+	t.string :answer
       t.timestamps null: false
     end
   end
