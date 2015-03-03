3316fe44d5957de22075c96ef61fb2413a30913e
yesnatalia <nataliaa.r@hotmail.com>
2015-03-02 20:20:25 -0600

    Update pirate.rb

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,5 @@
 class Pirate < ActiveRecord::Base
+  has_many :pirate_hunts
+  has_many :pirate_tasks, through :pirate_hunts #look at section 2.4 unsure if engine wants this
+  #http://guides.rubyonrails.org/association_basics.html
 end
