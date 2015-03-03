33e266b645aca39a0505c7cd5e7059f7ec42a32e
Jake Armando Balderama <balderaj@southwestern.edu>
2015-03-02 20:26:19 -0600

    Seeds Attempt
    First attempt at seeds: probably needs some more tries

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+pirate.create(:display_name => "Harry Potter")
+
+hunt.create(:title => "Chamber of Secrets",
+				:author => "Harry Potter",
+				:active => "True",
+				:published => "True",
+				:public => "True",
+				:start_location => "library",
+				:description => "Theres a snake in there")
+
+task.create(:points => "100",
+				:clue => "Do this",
+				:completed => "false")
+
+photo.create(:prompt => "Picture of a fang",
+				:submitted => "False")
+
+gps.create(:directions => "Underneathe the girls bathroom",
+				:correct_coordinates => "0.0",
+				:submitted_cooridnates => "0.0")
+question.create(:question => "Worst house?",
+					:right_answer => "Syltherin"
+					:submitted_answer => "Gryffindor")
+
+pirate_hunt.create()
+
+pirate_task.create()
