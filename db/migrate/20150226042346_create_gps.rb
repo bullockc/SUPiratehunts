e12f25e1073c31efda6806dbf3b267ed46e3a6bc
yesnatalia <nataliaa.r@hotmail.com>
2015-03-02 20:45:35 -0600

    Update 20150226042346_create_gps.rb

--- db/migrate/20150226042346_create_gps.rb
+++ db/migrate/20150226042346_create_gps.rb
@@ -2,8 +2,7 @@ class CreateGps < ActiveRecord::Migration
   def change
     create_table :gps do |t|
     	t.string :directions
-    	t.string :correct_coordinates
-    	t.string :submitted_coordinates
+    	t.string :coordinates
       	t.timestamps null: false
     end
   end
