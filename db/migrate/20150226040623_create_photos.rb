bd93964bb9ef47d129a82e988e63f5d742f7c560
yesnatalia <nataliaa.r@hotmail.com>
2015-03-02 20:49:01 -0600

    Update 20150226040623_create_photos.rb

--- db/migrate/20150226040623_create_photos.rb
+++ db/migrate/20150226040623_create_photos.rb
@@ -2,7 +2,6 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :prompt
-      t.boolean :submitted
       t.timestamps null: false
     end
   end
