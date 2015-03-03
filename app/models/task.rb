5efbf82e90738b56aa58e196ff7bcbc6ee781274
yesnatalia <nataliaa.r@hotmail.com>
2015-03-02 20:21:10 -0600

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
 	belongs_to :hunt
-	has_many :photo, :class_name=> 'Photo'
+	has_many :photos, :class_name=> 'Photo'
 	has_many :questions, :class_name=> 'Question'
-	has_many :GPS, :class_name=> 'GPS'
+	has_many :GPSs, :class_name=> 'GPS'
 end
