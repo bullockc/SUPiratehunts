54993eafdb755331ac100abfe8384ab220af5511
Kristin Stuckey <stuckeyk@southwestern.edu>
2015-03-03 20:44:56 -0600

    added references to models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ActiveRecord::Base
-	belongs_to :hunt, index :true
-	has_many :photos, :class_name=> 'Photo'
-	has_many :questions, :class_name=> 'Question'
-	has_many :GPSs, :class_name=> 'GPS'
+	#I think 'index:true' syntax only applies if you declare the association in the migration file
+	belongs_to :hunt #, index :true
+	#no longer relevant because we decided to have one tasks model
+	#with fields for each type of answer.
+	#has_many :photos, :class_name=> 'Photo'
+	#has_many :questions, :class_name=> 'Question'
+	#has_many :GPSs, :class_name=> 'GPS'
 end
--- db/migrate/20150303005224_create_pirate_hunts.rb
+++ db/migrate/20150303005224_create_pirate_hunts.rb
@@ -2,7 +2,8 @@ class CreatePirateHunts < ActiveRecord::Migration
   def change
     create_table :pirate_hunts do |t|
       t.boolean :completed
-
+      t.references :pirate
+      t.references :hunt
       t.timestamps null: false
     end
   end
