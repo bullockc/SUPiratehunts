39e29b2016ddb4fc5bd6231ef6ee34f57f9db892
michaeldmorris <morrism@southwestern.edu>
2015-03-24 12:27:53 -0500

    YAML updates
    Removed GPS, photos, and questions YAML, updated fixtures for users and
    pirates

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,6 +5,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   
+
+  #may need to add all of the other fields from the schema here? not sure yet.
   test "user has email and password" do
 	user = User.create(email: 'user1@gmail.com', encrypted_password: '4p78D')
 	assert user.valid?, "User information not saved"
@@ -21,3 +23,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
 end
+
+
+#may need later
+#, created_at: '2015-03-24 17:12:02', updated_at: '2015-03-24 17:12:02', id: '987654321'
