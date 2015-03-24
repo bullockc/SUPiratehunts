76af058f2de0597758c51fd2e7b6df5995822fc7
Kristin Stuckey <stuckeyk@southwestern.edu>
2015-03-24 15:40:58 -0500

    Added dependency to Hunt model
    Pirate hunts belonged to a hunt but the corresponding has_many pirate
    hunts in Hunt was not declared; also added dependencies for destroy

--- app/models/hunt.rb
+++ app/models/hunt.rb
@@ -1,4 +1,5 @@
 class Hunt < ActiveRecord::Base #Singular because it is a class
-	has_many :tasks, :class_name=> 'Task'
-	belongs_to :pirate, :class_name=> 'Pirate' #This is for the author
+	has_many :tasks, :dependent => :destroy#, :class_name=> 'Task'
+	belongs_to :pirate#, :class_name=> 'Pirate' #This is for the author
+	has_many :pirate_hunts, :dependent => :destroy
 end
