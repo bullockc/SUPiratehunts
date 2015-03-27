7d9633914db06e57669a17237c76faadaaf354c3
Ross Warkentin <warkentr@southwestern.edu>
2015-03-27 15:23:21 -0500

    *:destroy

--- app/models/pirate_hunt.rb
+++ app/models/pirate_hunt.rb
@@ -2,5 +2,5 @@ class PirateHunt < ActiveRecord::Base #Singular because it is a class
 	#I think 'index:true' syntax only applies if you declare the association in the migration file
 	belongs_to :user #, index:true #this makes it a field in the table
 	belongs_to :hunt #, index:true #http://stackoverflow.com/questions/3401504/can-model-belong-to-two-other-models-and-have-a-nested-relationship
-	has_many :pirate_tasks, :dependent => destroy
+	has_many :pirate_tasks, :dependent => :destroy
   end
