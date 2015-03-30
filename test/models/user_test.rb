1e40ae6170832f100fb11f9ea77b56254c98be97
Brittany Pugh <pughb@southwestern.edu>
2015-03-29 21:50:45 -0500

    changing back user test and fixture file

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,19 +7,24 @@ class UserTest < ActiveSupport::TestCase
   
 
   #may need to add all of the other fields from the schema here? not sure yet.
-  test "user has email and password" do
-	user = User.create(email: 'user1@gmail.com', encrypted_password: 'password1')
+  test "user has display name, email and encrypted password" do
+	user = User.create(display_name: 'user', email: 'user1@gmail.com', encrypted_password: 'password1')
 	assert user.valid?, "User information not saved"
+  end
+ 
+ test "only display name entered" do
+	user = User.create(display_name: 'user')
+	assert_not user.valid?, "User email and password not entered"
   end
  
  test "only an email entered" do
 	user = User.create(encrypted_password: '4p78D')
-	assert_not user.valid?, "User email not entered"
+	assert_not user.valid?, "User display name and password not entered"
   end
  
-  test "only a password entered" do
+  test "only encrypted password entered" do
   	user = User.create(email: 'user1@gmail.com')
-	assert_not user.valid?, "User information not saved"
+	assert_not user.valid?, "User display name and email not entered"
   end
   
 end
