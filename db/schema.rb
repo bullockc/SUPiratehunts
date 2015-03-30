c7531d2d9c5e1b6034d86df6389ee9390c72a46b
michaeldmorris <morrism@southwestern.edu>
2015-03-29 21:28:57 -0500

    Updated Schema
    Removed the pirate_id from Users (somehow it got added in again)

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 20150317205210) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "pirate_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
