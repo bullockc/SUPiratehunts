c4c3d34eceb9afc8627a52bd88924f8e944f9b68
Jake Armando Balderama <balderaj@southwestern.edu>
2015-04-01 21:10:31 -0500

    Changes to test files
    For some reason not done on our branch, but needs to be pushed

--- test/models/pirate_hunt_test.rb
+++ test/models/pirate_hunt_test.rb
@@ -7,24 +7,24 @@ class PirateHuntTest < ActiveSupport::TestCase
 
   #test that the entry is valid when all parameters are entered
   test "pirate hunt complete" do
-  	pirateHunt = PirateHunt.create(completed: false, pirate: '#12421234', hunt: '#15221234')
+  	pirateHunt = PirateHunt.create(completed: false, user: 'user', hunt: 15221234)
   	assert pirateHunt.valid?, 'PirateHunt data not saved'
   end
 
   #test that the entry is invalid without a completed field
   test "pirate hunt without completion" do
-  	pirateHunt = PirateHunt.create(pirate: '#12421242', hunt: '#15221232')
+  	pirateHunt = PirateHunt.create(user: 'user', hunt: 15221232'
   	assert_not pirateHunt.valid?, 'Should be invalid without all parameters'
   end
 
-  #test that the entry is invalid without a pirate
+  #test that the entry is invalid without a user
   test "pirate hunt without pirate" do
-  	pirateHunt = PirateHunt.create(completed: false, hunt: '#15223342')
+  	pirateHunt = PirateHunt.create(completed: false, hunt: 15223342)
   	assert_not pirateHunt.valid?, 'Should be invalid without all parameters'
   end
   #test that the entry is invalid without a hunt
   test "pirate hunt without hunt" do
-  	pirateHunt = PirateHunt.create(completed: false, pirate: '#12421122')
+  	pirateHunt = PirateHunt.create(completed: false, user: 'user')
   	assert_not pirateHunt.valid?, 'Should be invalid without all parameters'
   end
 end
--- test/models/pirate_task_test.rb
+++ test/models/pirate_task_test.rb
@@ -7,37 +7,39 @@ class PirateTaskTest < ActiveSupport::TestCase
 
   #test that the entry is valid when all parameters are entered
   test "pirate task complete" do
-  	pirateTask = PirateTask.create(answer_uploaded: 'True', completed: 'True', pirate: '1234', task: '1423', hunt: '9876')
+  	pirateTask = PirateTask.create(answer_uploaded: true, completed: true, task: '1423', hunt: '9876')
   	assert pirateTask.valid?, 'PirateTask data not saved'
   end
 
   #test that the entry is invalid without an answer uploaded
   test "pirate task without answer" do
-  	pirateTask = PirateTask.create(completed: 'True', pirate: '1234', task: '1423', hunt: '9876')
+  	pirateTask = PirateTask.create(completed: true, task: '1423', hunt: '9876')
   	assert_not pirateTask.valid?, 'PirateTask data not saved'
   end
 
   #test that the entry is invalid without a completed
   test "pirate task without completed" do
-  	pirateTask = PirateTask.create(answer_uploaded: 'True', pirate: '1234', task: '1423', hunt: '9876')
+  	pirateTask = PirateTask.create(answer_uploaded: true, task: '1423', hunt: '9876')
   	assert_not pirateTask.valid?, 'PirateTask data not saved'
   end
 
+=begin
   #test that the entry is invalid without a pirate
   test "pirate task without pirate" do
   	pirateTask = PirateTask.create(answer_uploaded: 'True', completed: 'True', task: '1423', hunt: '9876')
   	assert_not pirateTask.valid?, 'PirateTask data not saved'
   end
+=end
 
   #test that the entry is invalid without a task
   test "pirate task without task" do
-  	pirateTask = PirateTask.create(answer_uploaded: 'True', completed: 'True', pirate: '1234', hunt: '9876')
+  	pirateTask = PirateTask.create(answer_uploaded: true, completed: true, hunt: '9876')
   	assert_not pirateTask.valid?, 'PirateTask data not saved'
   end
 
   #test that the entry is invalid without a hunt
   test "pirate task without hunt" do
-  	pirateTask = PirateTask.create(answer_uploaded: 'True', completed: 'True', pirate: '1234', task: '1423')
+  	pirateTask = PirateTask.create(answer_uploaded: true, completed: true, task: '1423')
   	assert_not pirateTask.valid?, 'PirateTask data not saved'
   end
 end
