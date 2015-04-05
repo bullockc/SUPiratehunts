c1688acd66900eea0ba2533eec7868654de1aa56
Rebecca L Wilson <rebecca.l.wilson@hotmail.com>
2015-04-05 14:18:45 -0500

    Leave hunt button pirate_hunts/show
    Added a leave hunt button that destroys pirate hunt and all associated
    pirate tasks, logic in pirate_hunts model ("dependent destroy")

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20150401154714) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "display_name"
+    t.string   "display_name",           default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -78,7 +78,6 @@ ActiveRecord::Schema.define(version: 20150401154714) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "pirate_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
