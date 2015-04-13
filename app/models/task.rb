c3f5c6947921873a9a47c9fb4af40952cafbc027
Christian Bullock <bullockc@southwestern.edu>
2015-04-13 16:54:18 -0500

    Validation
    You can no longer create a hunt without a title, nor a task without a
    prompt or point value.

--- app/models/task.rb
+++ app/models/task.rb
@@ -13,6 +13,9 @@ class Task < ActiveRecord::Base #Singular because it is a class
 	#has_many :questions, :class_name=> 'Question'
 	#has_many :GPSs, :class_name=> 'GPS'
 
+        validates :points, presence: true
+	validates :prompt, presence: true
+
   # Create all PirateTasks for existing PirateHunts related
   # to this Task
   def create_pirate_tasks
