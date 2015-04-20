8e5290b8e6f89515cf147e4b19039b8c7b4c16b4
Kristin Stuckey <stuckeyk@southwestern.edu>
2015-04-20 10:28:44 -0500

    more carousel changes
    added randomize of photos, min number of items to display, changed
    approval partial to take medium size photos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,9 @@ $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "slide",
     animationLoop: false,
-    itemWidth: 210,
-    itemMargin: 5
+    minItems: 3,
+    itemWidth: 200,
+    itemMargin: 5,
+    randomize: true
   });
 });
